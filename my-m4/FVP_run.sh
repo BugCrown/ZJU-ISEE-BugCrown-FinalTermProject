7f251916cf5355d6a49984bd7cf3cae9eda94292
BugCrown <96652063+BugCrown@users.noreply.github.com>
2023-02-08 10:59:24 +0800

    Delete my-m4 directory

--- my-m4/FVP_run.sh
+++
@@ -1,6 +0,0 @@
-# Run demo executable on the FVP
-FVP_Corstone_SSE-300_Ethos-U55 -C cpu0.CFGDTCMSZ=15 \
--C cpu0.CFGITCMSZ=15 -C mps3_board.uart0.out_file=\"-\" -C mps3_board.uart0.shutdown_tag=\"EXITTHESIM\" \
--C mps3_board.visualisation.disable-visualisation=1 -C mps3_board.telnetterminal0.start_telnet=0 \
--C mps3_board.telnetterminal1.start_telnet=0 -C mps3_board.telnetterminal2.start_telnet=0 -C mps3_board.telnetterminal5.start_telnet=0 \
-./build/demo
