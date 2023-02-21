ebe1264b827d3aff5445e4f0cc9bb780418ed1c3
BugCrown <96652063+BugCrown@users.noreply.github.com>
2023-02-21 16:59:39 +0800

    Add files via upload

---
+++ TFLM/README.md
@@ -0,0 +1,14 @@
+# wdcnn
+文件夹内包含了STM32CubeMX工程文件和由其生成的MDK5工程。  
+MDK工程使用的是STM32CubeMX自动生成的HAL库文件，根据注释修改和添加代码。
+请注意，这是CubeAI里的TFLM部署方案，CubeAI只提供了C++到C的接口。
+## 环境
+### Windows
++ STM32CubeMX：V6.7.0
++ X-Cube-AI：V7.3.0
++ MDK：μVersion V5.38.0.0
++ ARM Compiler：V5.05
++ C Standard：C90
++ C++ Standard：C++ 11
++ ARM CMSIS：V5.9.0
++ ARM CMSIS-NN：V2.7.2
---
+++ TFLM/wdcnn/Inc/network_tflite_data.h
@@ -0,0 +1,32 @@
+/* Generated file - v1.0
+ *
+ * Original file         : WDCNNQuant_v1.tflite
+ */
+
+#ifndef __NETWORK_TFLITE_DATA_H__
+#define __NETWORK_TFLITE_DATA_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern const uint8_t g_tflm_network_model_data[];
+
+extern const int g_tflm_network_model_data_len;
+
+#undef TFLM_NETWORK_TENSOR_AREA_SIZE
+#define TFLM_NETWORK_TENSOR_AREA_SIZE 13312
+
+#undef TFLM_NETWORK_NAME
+#define TFLM_NETWORK_NAME "wdcnn"
+
+#undef TFLM_NETWORK_FILE_NAME
+#define TFLM_NETWORK_FILE_NAME "WDCNNQuant_v1"
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __NETWORK_TFLITE_DATA_H__ */
